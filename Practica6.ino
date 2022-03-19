29e301eff439c4e48bba24be116573511d204321
EL3S4 <mdasjupiter@protonmail.com>
2022-03-19 10:26:11 +0100

    actu

--- Practica6.ino
+++ Practica6.ino
@@ -11,8 +11,32 @@ void loop(){
     
     i=0;
     while(Serial.available()){
-        s[i++] = Serial.read();
+        cadena[i++] = Serial.read();
         delay(1);
     }
-    s[i]=0;
+    cadena[i]=0;
+    if(i>0){
+        String salida;
+        for(int j = 0; j < 100; j++){
+            char caracter = cadena[j];
+            if (caracter == 0){break;}
+            salida = salida + String(caracter);
+            if (caracter == char("a")){
+                salida = salida + String(caracter);
+            }
+            if (caracter == char("e")){
+                salida = salida + String(caracter);
+            }
+            if (caracter == char("i")){
+                salida = salida + String(caracter);
+            }
+            if (caracter == char("o")){
+                salida = salida + String(caracter);
+            }
+            if (caracter == char("u")){
+                salida = salida + String(caracter);
+            }
+        }
+        Serial.print(salida);
+    }
 }
